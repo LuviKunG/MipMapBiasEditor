84dd8ceb9237bb36158cd322bb7bbe1221919642
Luvi Lennox <luvikung@gmail.com>
2019-09-17 01:22:48 +0700

    add a project.

---
+++ Editor/MipMapBiasMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace LuviKunG.MipMapBias
+{
+    public static class MipMapBiasMenu
+    {
+        private static Object[] selection;
+
+        [MenuItem("Assets/LuviKunG/Open Mipmap Bias Window", true)]
+        public static bool ValidateSetBias()
+        {
+            selection = Selection.GetFiltered(typeof(Texture), SelectionMode.DeepAssets);
+            return selection.Length > 0;
+        }
+
+        [MenuItem("Assets/LuviKunG/Open Mipmap Bias Window", false)]
+        public static void SetBias()
+        {
+            MipMapBiasEditor.OpenWindowWithSelection(selection);
+        }
+    }
+}
