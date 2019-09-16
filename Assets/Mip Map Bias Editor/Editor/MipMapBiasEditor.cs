284fe4e093d5f9c30bb58767af68682f48f50065
Luvi Lennox <luvikung@gmail.com>
2019-09-17 01:22:48 +0700

    add a project.

---
+++ Assets/Mip Map Bias Editor/Editor/MipMapBiasEditor.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace LuviKunG.MipMapBias
+{
+    public sealed class MipMapBiasEditor : EditorWindow
+    {
+        public enum BiasLevel : int
+        {
+            Normal,
+            High,
+            VeryHigh,
+        }
+
+        private static readonly GUIContent CONTENT_MIP_MAP_BIAS_LEVEL = new GUIContent("Mipmap Bias Level", "This will set all your selection textures mipmap bias depend on level.\nNormal = 0\nHigh = -0.5\nVery High = -1.5");
+
+        public static MipMapBiasEditor OpenWindowWithSelection(Object[] objects)
+        {
+            MipMapBiasEditor window = GetWindow<MipMapBiasEditor>(true, "Mipmap Bias", true);
+            window.SetSelection(objects);
+            window.Show();
+            return window;
+        }
+
+        private List<Texture> list = new List<Texture>();
+        private Texture texture;
+        private Vector2 scrollview;
+        private BiasLevel biasLevel;
+
+        public void SetSelection(Object[] objects)
+        {
+            foreach (Texture texture in objects)
+                list.Add(texture);
+        }
+
+        private void OnGUI()
+        {
+            if (list != null && list.Count > 0)
+            {
+                biasLevel = (BiasLevel)EditorGUILayout.EnumPopup(CONTENT_MIP_MAP_BIAS_LEVEL, biasLevel);
+                if (GUILayout.Button("Update Mipmap Bias"))
+                {
+                    float currentBiasLevel = GetBiasLevelValue(biasLevel);
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        string path = AssetDatabase.GetAssetPath(list[i]);
+                        (AssetImporter.GetAtPath(path) as TextureImporter).mipMapBias = currentBiasLevel;
+                        AssetDatabase.ImportAsset(path);
+                    }
+                }
+                using (var scrollViewScope = new EditorGUILayout.ScrollViewScope(scrollview))
+                {
+                    scrollview = scrollViewScope.scrollPosition;
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        if (list[i] == null) list.RemoveAt(i--);
+                        else list[i] = (Texture)EditorGUILayout.ObjectField(GUIContent.none, list[i], typeof(Texture), false, GUILayout.Height(EditorGUIUtility.singleLineHeight));
+                    }
+                }
+            }
+            else
+            {
+                EditorGUILayout.HelpBox("Please using Assets Menu to configure mipmap bias.", MessageType.Warning, true);
+            }
+        }
+
+        private float GetBiasLevelValue(BiasLevel level)
+        {
+            switch (level)
+            {
+                case BiasLevel.Normal: return 0.0f;
+                case BiasLevel.High: return -0.5f;
+                case BiasLevel.VeryHigh: return -1.5f;
+                default: return 0.0f;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,40 @@
+# MipMapBiasEditor
+Mip Map Bias Editor is an Unity Editor plugins that can perform Mip Map Bias quality selection and batching all textures in the selected folder of project. Created by Thanut Panichyotai (@[LuviKunG]((https://github.com/LuviKunG)))
+
+## How to use?
+
+By right click on the folder or file in your Unity Project windows and select **LuviKunG/Open Mip Map Bias Window** will open the editor window.
+
+**Note that**: It will show as blank and cannot do action when the selecting folder or file isn't texture.
+
+In the window, you can select quality of the Mip Map Bias of the texture and apply them all.
+
+## How to install?
+
+### UPM Install via manifest.json
+
+Locate to your Unity Project. In *Packages* folder, you will see a file named **manifest.json**. Open it with your text editor (such as Notepad++ or Visual Studio Code or Legacy Notepad)
+
+Then merge this json format below.
+
+(Do not just copy & paste the whole json! If you are not capable to merge json, please using online JSON merge tools like [this](https://tools.knowledgewalls.com/onlinejsonmerger))
+
+```json
+{
+  "dependencies": {
+    "com.luvikung.mipmapbias": "https://github.com/LuviKunG/MipMapBiasEditor.git#1.0.0"
+  }
+}
+```
+
+If you want to install the older version, please take a look at release tag in this git, then change the path after **#** to the version tag that you want.
+
+### Unity UPM Git Extension
+
+If you doesn't have this package before, please redirect to this git [https://github.com/mob-sakai/UpmGitExtension](https://github.com/mob-sakai/UpmGitExtension) then follow the instruction in README.md to install the **UPM Git Extension** to your Unity.
+
+If you already installed. Open the **Package Manager UI**, you will see the git icon around the bottom left connor, Open it then follow the instruction using this git URL to perform package install.
+
+![Install LuviConsole with UPM Git Extension](images/install01.png)
+
+Make sure that you're select the latest version.
